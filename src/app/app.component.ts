2e03b0fedf2b39cc56c6b3671364fc02e961ade0
vssoares <vsss23@hotmail.com>
2020-07-26 15:13:01 -0300

    All Jokes and Random Joke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ChuckNorrisApi';
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  show: boolean = true;
+
+  api = [];
+  randomJoke = [];
+
+  async fetchApi(){
+    await fetch('https://api.icndb.com/jokes')
+      .then((r) => r.json())
+      .then((r) =>{
+        this.api = r.value;
+        this.show = true
+      })
+  }
+
+  async fetchRandomJoke(){
+    await fetch('http://api.icndb.com/jokes/random/')
+      .then((r) => r.json())
+      .then((r) =>{
+        this.randomJoke = r.value;
+        this.show = false
+      })
+  }
+  
+  ngOnInit(): void {
+    this.fetchApi();
+    console.log(this.show);
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatSliderModule} from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
